15dcbe458e330c58f9160d41adc6b2612d2b3497
samuel <samiehave9@gmail.com>
2024-08-09 04:13:41 +0300

    Corrections

--- testin.rb
+++ testin.rb
@@ -4,7 +4,7 @@ mutable = Array.new(3, Array(2))
 # [[nil, nil], [nil, nil], [nil, nil]]
 mutable[0][0] = 1000
 # [[1000, nil], [1000, nil], [1000, nil]]
-mutable 
+# mutable 
 # [[1000, nil], [1000, nil], [1000, nil]]
 
 # immutable
@@ -12,7 +12,7 @@ immutable = Array.new(3) { Array.new(2) }
 # [[nil, nil], [nil, nil], [nil, nil]]
 immutable[0][0] = 1000
 # => 1000
-immutable
+#immutable
 # [[1000, nil], [nil, nil], [nil, nil]]
 # Changing the value of the first element of the first nested array does not affect the other nested arrays.
 
@@ -36,7 +36,7 @@ test_scores << [100, 99, 98, 97]
 # => [[97, 76, 79, 93], [79, 84, 76, 79], [88, 67, 64, 76], [94, 55, 67, 81], [100, 99, 98, 97]]
 test_scores[0].push(100)
 # => [97, 76, 79, 93, 100]
-test_scores
+# test_scores
 # => [[97, 76, 79, 93, 100], [79, 84, 76, 79], [88, 67, 64, 76], [94, 55, 67, 81], [100, 99, 98, 97]]
 
 # Removing elements
@@ -45,7 +45,7 @@ test_scores.pop
 # => [100, 99, 98, 97]
 test_scores[0].pop
 # => 100
-test_scores
+# test_scores
 # => [[97, 76, 79, 93], [79, 84, 76, 79], [88, 67, 64, 76], [94, 55, 67, 81]]
 
 # Iterating over nested arrays
